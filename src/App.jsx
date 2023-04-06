76ecaa662930c0b2441698400cc179ed4e36ce3d
ogzCode <oguzguc44@gmail.com>
2023-04-06 21:06:10 +0300

    Global redux and Tailwind directives created. Main application written

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TodoApp from './components/TodoApp';
+
+function App() {
+  return (
+    <div className="h-screen bg-gradient-to-r from-[#EC6F66] to-[#F3A183] flex justify-center items-center">
+      <TodoApp/>
+    </div>
+  )
+}
+
+export default App
